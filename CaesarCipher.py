739b88fd0a848d42430fedfcf784d538560e334d
gugly <liamwatford@protonmail.com>
2022-11-26 19:10:08 +0100

    added resilience, but incriesed time complexity

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -3,11 +3,12 @@ import string
 
 alphabet = string.ascii_lowercase + string.ascii_lowercase
 
-text = list(input("input text: \n"))
+text = list(input("input text: \n").lower())
 
-action = input("encrypt, decipher or exit: \n").lower()
+action = input("encrypt or decipher \n").lower()
 
-shiftNum = int(input("number of  shifts \n"))%25
+if action == "encrypt":
+    shiftNum = int(input("number of  shifts \n"))%25
 
 
 run = True
@@ -15,18 +16,14 @@ run = True
 while run == True:
     if action == "encrypt":
         for index, letter in enumerate(text):
-            if letter == " ":
-                text[index] = " "
-            else:
+            if letter.isalpha():
                 newLetter = alphabet.index(text[index]) + shiftNum
                 text[index] = alphabet[newLetter]
         print ("".join(map(str,text))) 
         run = False
     if action == "decipher":
         for index, letter in enumerate(text):
-            if letter == " ":
-                text[index] = " "
-            else:
+            if letter.isalpha():
                 newLetter = alphabet.index(text[index]) - shiftNum
                 text[index] = alphabet[newLetter]
         print ("".join(map(str,text))) 
@@ -34,10 +31,9 @@ while run == True:
     if action != "encrypt" and action != "decipher":
         tryAgain = input("something went wrong, try again? y/n \n")
         if tryAgain == "y":
-            text = list(input("input text: \n"))
-            
+            text = list(input("input text: \n").lower())
             action = input("encrypt, decipher or exit: \n").lower()
-
-            shiftNum = int(input("number of  shifts \n"))%25
+            if action == "encrypt":
+                shiftNum = int(input("number of  shifts \n"))%25
         else:
             run = False
